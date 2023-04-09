d9d3bd9f2d27db31e3b7a6352ebe958ddda32d2d
spiider <spiider.spiderco@gmail.com>
2023-04-09 17:40:57 -0400

    Made better

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require("express");
+const path = require("path");
 
 const port = 6969;
 const app = express();
 
 app.use("/", require("./src/routing/home"));
-app.use("/test", require("./src/routing/test"));
+app.use("/obfuscation", require("./src/routing/obfuscation"));
 app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.listen(port, function () {
